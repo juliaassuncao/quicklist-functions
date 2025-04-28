cc10cefc4e79d0f85796f4989199087ef082de2b
Julia Assunção <juliaassuncao256@gmail.com>
2025-04-28 16:08:09 -0300

    chore: criação de estruturas vazias para createNewItem, addItemToList e clearInput

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log("Estou funcionando!")
+function createNewItem() {
+    // TO-DO: implementar criação de novos itens
+}
+
+function addItemToList() {
+    // TO-DO: implementar funcionalidade de adicionar itens à lista
+}
+
+function clearInput() {
+    // TO-DO: implementar funcionalidade de limpar o input
+}
