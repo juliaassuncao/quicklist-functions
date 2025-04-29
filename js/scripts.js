7519d93018d4706c982b56bbab75e3e76161a05d
Julia Assunção <juliaassuncao256@gmail.com>
2025-04-29 11:38:27 -0300

    feat: finalização de createNewItem

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,28 @@ const form = document.querySelector("form")
 const input = document.getElementById("new-item")
 const listContainer = document.querySelector(".list-container")
 
-function createNewItem() {
+function createNewItem(itemText) {
+
+    const itemId = "item-" + (listContainer.children.length + 1)
 
     const item = document.createElement("div")
     item.classList.add("list-item")
+
+    const checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    checkbox.id = itemId
+
+    const label = document.createElement("label")
+    label.classList.add("list-item")
+    label.htmlFor = itemId
+    label.textContent = itemText
+
+    const trashIcon = document.createElement("i")
+    trashIcon.classList.add("hgi", "hgi-stroke", "hgi-delete-02", "remove-button")
+
+    item.appendChild(checkbox)
+    item.appendChild(label)
+    item.appendChild(trashIcon)
 
     return item
 }
@@ -25,7 +43,7 @@ form.onsubmit = (event) => {
     const itemText = input.value.trim()
 
     if (itemText !== "") {
-        const newItem = createNewItem()
+        const newItem = createNewItem(itemText)
         addItemToList(newItem)
         clearInput()
     }
